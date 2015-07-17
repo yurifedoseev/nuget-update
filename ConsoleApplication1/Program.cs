591d3533751e8f20a69b68cedc3090967a830611
yurifedoseev <fedoseev@moedelo.org>
2015-07-17 19:37:52 +0300

    regexp!

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ConsoleApplication1
@@ -10,6 +12,23 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            string folderLocation = args[0];
+            string dllName = args[1].Replace(".", "\\.");
+            string dllVersion = args[2];
+
+            string[] csproj = Directory.GetFiles(folderLocation, "*.csproj", SearchOption.AllDirectories);
+
+            foreach (string filePath in csproj)
+            {
+                string csProj = File.ReadAllText(filePath);
+                Regex regExp = new Regex(string.Format(@"packages\\{0}[^\\]*\\", dllName));
+
+                foreach (Match match in regExp.Matches(csProj))
+                {
+                    Console.Out.WriteLine(match.Value);
+                }
+
+            }
         }
     }
 }
